1e35ff0b0fa36db2cb318ce402d01a606bc085b8
Astle-Antony <astle123r5@gmail.com>
2025-11-10 23:27:26 +0530

    Day-8 Postional Arguments and Keyword Arguments

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,24 @@
+def greet():
+    print("welcome")
+    print("Hope you're doing well!")
+    print("it's been a pleasure")
+
+greet()
+
+
+# Function that allows input
+
+def greet_with_name(name):
+    print(f"welcome, {name}")
+    print("Hope you're doing well!")
+    print("it's been a pleasure")
+
+greet_with_name("Astle")
+
+
+
+#  Difference between parameter and Arguments
+'''
+The name is parameter. When we create a function with user-defined values those are known as parameters. 
+The Astle is argument. when we pass a value to that variable it's known as argument.
+'''
